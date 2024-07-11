6909a2a06ac30f4419dca9fccf12580172d2e825
Trent Avilla <131209117+vhalen0515@users.noreply.github.com>
2024-07-10 18:51:26 -0700

    fixed image url

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -7,7 +7,7 @@ export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: '',
         bottomText: '',
-        randomImage: 'http://i.imgflip.com/1bij.jpg'
+        randomImage: 'https://i.imgflip.com/1bij.jpg'
     })
 
     const [allMemes, setAllMemes] = React.useState([])
