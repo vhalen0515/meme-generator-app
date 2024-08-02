43c7e177c0255a2b4a8814f3e3918553b739874b
Trent Avilla <131209117+vhalen0515@users.noreply.github.com>
2024-08-01 19:30:00 -0700

    Deleted header text

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,6 @@ export default function Header() {
         <header>
             <img src="./troll-face.png" className="header--img" />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
